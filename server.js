436c6489ae7a8e6f4860c2829005f96edbd58273
2myemy <lss8340@gmail.com>
2023-08-30 14:16:10 -0700

    Revert "Revert "Changed proxy""
    This reverts commit ad4b8a998cac3e0731e19008de6092f96bde2887.

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const router = require("./route");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use("/", router);
 
 // app.get('/', (req, res) => {
 //   res.header("Access-Control-Allow-Origin", "*");
